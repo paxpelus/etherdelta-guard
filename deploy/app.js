d263bb34d78de9cb1a8916f4fff800e818580fb4
paxpelus <panagiotis.iatrou@gmail.com>
2017-09-22 12:08:53 +0000

    Added chrom store api & zip creation

--- deploy/app.js
+++ deploy/app.js
@@ -3,6 +3,17 @@ const app = express()
 var request = require('sync-request');
 var md5 = require('md5');
 var fs = require('fs');
+var fs2 = require('q-io/fs');
+var EasyZip = require('easy-zip').EasyZip;
+var WebstoreApi = require('chrome-store-api').Webstore;
+var TokenManager = require('chrome-store-api').TokenManager;
+
+var code = 'app-code';
+var clientId = 'your-client-id';
+var clientSecret = 'your-client-secret';
+
+var tokenManager = new TokenManager(code, clientId, clientSecret);
+var api = new WebstoreApi(tokenManager);
 
 var urls = [
     'https://raw.githubusercontent.com/etherdelta/etherdelta.github.io/master/index.html',
@@ -46,16 +57,49 @@ app.get('/', function (req, res) {
                 url: urls[i].replace('https://raw.githubusercontent.com/etherdelta/etherdelta.github.io/master/', 'https://etherdelta.com/'),
                 md5: checkFile(urls[i])});
         }
-    
+
+        //Create hashes.js file with appropriate md5 hashes
         fs.writeFile("../hashes.js", "var hashes = " + JSON.stringify(md5), function(err) {
             if(err) {
                 return console.log(err);
             }
-        
-            console.log("The file was saved!");
-        }); 
 
-        res.send(md5)
+            console.log("The file was saved!");
+        });
+
+        // Create the zip file for uploading to chrome store
+        var zip = new EasyZip();
+        //batch add files
+        var files = [
+        	{source : '../background/background.js',target:'background/background.js'},
+        	{source : '../content_scripts/script.js',target:'content_scripts/script.js'},
+        	{source : '../icons/icon-error.png',target:'icons/icon-error.png'},
+        	{source : '../icons/icon-inactive.png',target:'icons/icon-inactive.png'},
+        	{source : '../icons/icon.png',target:'icons/icon.png'},
+        	{source : '../libraries/md5.js',target:'libraries/md5.js'},
+        	{source : '../hashes.js',target:'hashes.js'},
+        	{source : '../manifest.json',target:'manifest.json'},
+        	{source : '../popup.js',target:'popup.js'},
+        	{source : '../popup.html',target:'popup.html'},
+        	{source : '../README.md',target:'README.md'}
+        ];
+
+        zip.batchAdd(files,function(){
+        	zip.writeToFile('extension.zip');
+        });
+
+        fs2.read('extension.zip', 'b')
+          .then(function (blob) {
+              return api.update('extension-id', blob);
+          })
+          .then(function (data) {
+              console.log(data); // item info
+              res.send(md5)
+          })
+          .catch(function (err) {
+              console.log(err);
+              res.send("Error updating chrome store")
+          });
     } else {
         res.send("Unauthorized")
     }
@@ -65,7 +109,7 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-function checkFile (url) {    
+function checkFile (url) {
     var res = request('GET', url);
     return md5(res.getBody());
 }
